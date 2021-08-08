36dc825de45f56f73517ecfd3e1a7a2d5b7bd2d7
malijani <alijanialijanvandmohammad@gmail.com>
2021-08-08 14:25:05 +0430

    Updated routes

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,8 @@
 use App\User;
 use Illuminate\Support\Facades\Auth;
 use \Illuminate\Support\Facades\Route;
-use \Unisharp\LaravelFilemanager\Lfm;
+use UniSharp\LaravelFilemanager\Middlewares\CreateDefaultFolder;
+use UniSharp\LaravelFilemanager\Middlewares\MultiUser;
 
 /*TODO : REMOVE THIS TEST LOGIN*/
 Route::get('custom-login/{mobile}', function (int $mobile) {
@@ -130,11 +131,104 @@ Route::group(['prefix' => 'admin', 'middleware' => ['web', 'auth', 'auth.admin']
     /*FILE MANAGER*/
     Route::view('files', 'admin.file-manager.index')->name('admin.fm-frame');
     Route::group(['prefix' => 'file-manager'], function () {
-        Lfm::routes();
+        $middleware = [ CreateDefaultFolder::class, MultiUser::class ];
+        $as = 'unisharp.lfm.';
+        $namespace = '\\UniSharp\\LaravelFilemanager\\Controllers\\';
+
+        Route::group(compact('middleware', 'as', 'namespace'), function () {
+
+            // display main layout
+            Route::get('/', [
+                'uses' => 'LfmController@show',
+                'as' => 'show',
+            ]);
+
+            // display integration error messages
+            Route::get('/errors', [
+                'uses' => 'LfmController@getErrors',
+                'as' => 'getErrors',
+            ]);
+
+            // upload
+            Route::any('/upload', [
+                'uses' => 'UploadController@upload',
+                'as' => 'upload',
+            ]);
+
+            // list images & files
+            Route::get('/jsonitems', [
+                'uses' => 'ItemsController@getItems',
+                'as' => 'getItems',
+            ]);
+
+            Route::get('/move', [
+                'uses' => 'ItemsController@move',
+                'as' => 'move',
+            ]);
+
+            Route::get('/domove', [
+                'uses' => 'ItemsController@domove',
+                'as' => 'domove'
+            ]);
+
+            // folders
+            Route::get('/newfolder', [
+                'uses' => 'FolderController@getAddfolder',
+                'as' => 'getAddfolder',
+            ]);
+
+            // list folders
+            Route::get('/folders', [
+                'uses' => 'FolderController@getFolders',
+                'as' => 'getFolders',
+            ]);
+
+            // crop
+            Route::get('/crop', [
+                'uses' => 'CropController@getCrop',
+                'as' => 'getCrop',
+            ]);
+            Route::get('/cropimage', [
+                'uses' => 'CropController@getCropimage',
+                'as' => 'getCropimage',
+            ]);
+            Route::get('/cropnewimage', [
+                'uses' => 'CropController@getNewCropimage',
+                'as' => 'getCropnewimage',
+            ]);
+
+            // rename
+            Route::get('/rename', [
+                'uses' => 'RenameController@getRename',
+                'as' => 'getRename',
+            ]);
+
+            // scale/resize
+            Route::get('/resize', [
+                'uses' => 'ResizeController@getResize',
+                'as' => 'getResize',
+            ]);
+            Route::get('/doresize', [
+                'uses' => 'ResizeController@performResize',
+                'as' => 'performResize',
+            ]);
+
+            // download
+            Route::get('/download', [
+                'uses' => 'DownloadController@getDownload',
+                'as' => 'getDownload',
+            ]);
+
+            // delete
+            Route::get('/delete', [
+                'uses' => 'DeleteController@getDelete',
+                'as' => 'getDelete',
+            ]);
+
+        });
     });
-
-
 });
+
 
 Route::group(['prefix' => 'files', 'as' => 'files.', 'middleware' => ['web', 'auth']], function () {
     Route::get('ticket/{id}/{type}', 'File\TicketFileController@show')->name('ticket-files');
