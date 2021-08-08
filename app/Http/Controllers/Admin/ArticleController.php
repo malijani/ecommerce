48401f08ac5c98f561f224ae3f9978529ddbc1eb
malijani <alijanialijanvandmohammad@gmail.com>
2021-08-08 14:20:49 +0430

    Updated admin article controller

--- app/Http/Controllers/Admin/ArticleController.php
+++ app/Http/Controllers/Admin/ArticleController.php
@@ -8,15 +8,16 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Str;
+use Illuminate\View\View;
 use Intervention\Image\ImageManager;
 
 class ArticleController extends Controller
 {
     /**
      * Display a listing of the resource.
-     *
+     * @return View
      */
-    public function index()
+    public function index() : View
     {
         $title = 'مدیریت مقالات';
         $articles = Article::withoutTrashed()
@@ -25,18 +26,17 @@ class ArticleController extends Controller
             ->orderBy('sort', 'ASC')
             ->get();
 
-        return view('admin.article.index',
-            [
-                'title' => $title,
-                'articles' => $articles,
-            ]);
+        return view('admin.article.index', [
+            'title' => $title,
+            'articles' => $articles,
+        ]);
     }
 
     /**
      * Show the form for creating a new resource.
-     *
+     * @return View
      */
-    public function create()
+    public function create() : View
     {
         $title = 'ثبت مقاله جدید';
         $categories = Category::withoutTrashed()
@@ -62,7 +62,7 @@ class ArticleController extends Controller
     /**
      * Store a newly created resource in storage.
      *
-     * @param \Illuminate\Http\Request $request
+     * @param Request $request
      */
     public function store(Request $request)
     {
@@ -78,17 +78,17 @@ class ArticleController extends Controller
             'long_text' => ['required', 'min:10', 'max:40000'],
             'period' => ['required', 'numeric'],
             'status' => ['required', 'numeric'],
-            'pic'=>['nullable', 'mimes:jpg,jpeg,png,gif', 'max:10240'],
-            'pic_alt'=>['nullable','min:2', 'max:70'],
+            'pic' => ['nullable', 'mimes:jpg,jpeg,png,gif', 'max:10240'],
+            'pic_alt' => ['nullable', 'min:2', 'max:70'],
         ]);
 
         $pic = imageUploader($request, 'pic', 'article');
 
         Article::query()->create(array_merge(
             $request->except('pic', 'title_en'),
-            ['pic'=>$pic],
-            ['title_en'=>Str::slug($request->input('title_en'))],
-            ['user_id'=> Auth::id()]
+            ['pic' => $pic],
+            ['title_en' => Str::slug($request->input('title_en'))],
+            ['user_id' => Auth::id()]
         ));
 
         return redirect(route('articles.index'))->with('success', 'مقاله با موفقیت ثبت شد');
@@ -135,7 +135,7 @@ class ArticleController extends Controller
             'article' => $article,
             'categories' => $categories,
             'title' => $title,
-            'articles'=>$articles,
+            'articles' => $articles,
         ]);
     }
 
@@ -153,21 +153,21 @@ class ArticleController extends Controller
             'category_id' => ['required', 'numeric'],
             'before' => ['required', 'numeric'],
             'after' => ['required', 'numeric'],
-            'title' => ['required', 'min:2', 'max:70', 'unique:articles,title,'.$article->id],
-            'title_en' => ['required', 'min:2', 'max:70', 'unique:articles,title_en,'.$article->id],
+            'title' => ['required', 'min:2', 'max:70', 'unique:articles,title,' . $article->id],
+            'title_en' => ['required', 'min:2', 'max:70', 'unique:articles,title_en,' . $article->id],
             'keywords' => ['nullable', 'min:2', 'max:70'],
             'description' => ['nullable', 'min:3', 'max:255'],
             'short_text' => ['nullable', 'min:3', 'max:255'],
             'long_text' => ['required', 'min:10', 'max:40000'],
             'period' => ['required', 'numeric'],
             'status' => ['required', 'numeric'],
-            'pic'=>['nullable', 'mimes:jpg,jpeg,png,gif', 'max:10240'],
-            'pic_alt'=>['nullable','min:2', 'max:70'],
+            'pic' => ['nullable', 'mimes:jpg,jpeg,png,gif', 'max:10240'],
+            'pic_alt' => ['nullable', 'min:2', 'max:70'],
         ]);
 
 
-        if($request->has('delete_pic') && $request->input('delete_pic')=="on"){
-            if(!is_null($article->pic)){
+        if ($request->has('delete_pic') && $request->input('delete_pic') == "on") {
+            if (!is_null($article->pic)) {
                 unlink(public_path($article->pic));
             }
         }
@@ -176,16 +176,16 @@ class ArticleController extends Controller
         $pic = imageUploader($request, 'pic', 'article');
 
 
-        if(!is_null($pic) && !is_null($article->pic)){
+        if (!is_null($pic) && !is_null($article->pic)) {
             unlink(public_path($article->pic));
         }
 
         $article->update(array_merge(
-            $request->except(['pic','title_en']),
-            ['pic'=>$pic ?? $article->pic],
-            ['title_en'=>Str::slug($request->input('title_en'))]
+            $request->except(['pic', 'title_en']),
+            ['pic' => $pic ?? $article->pic],
+            ['title_en' => Str::slug($request->input('title_en'))]
         ));
-        return redirect(route('articles.index'))->with('success', ' مقاله '. $article->title.' با موفقیت به روز رسانی شد ');
+        return redirect(route('articles.index'))->with('success', ' مقاله ' . $article->title . ' با موفقیت به روز رسانی شد ');
 
     }
 
@@ -199,11 +199,9 @@ class ArticleController extends Controller
         $article = Article::withoutTrashed()->findOrFail($id);
         $article->delete();
 //        return redirect(route('articles.index'))->with('success', ' مقاله '.$article->title . ' با موفقیت حذف شد! ');
-        return response()->json('مقاله '.$article->title. ' با موفقیت حذف شد');
+        return response()->json('مقاله ' . $article->title . ' با موفقیت حذف شد');
 
     }
-
-
 
 
 }
